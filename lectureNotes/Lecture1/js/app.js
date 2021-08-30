febc1dddf4ec4e18218bc9bcecbc07410dbf8554
ACutler-S <ancutler@iu.edu>
2021-08-29 23:41:35 -0400

    Created a Notes folder

---
+++ lectureNotes/Lecture1/js/app.js
@@ -0,0 +1,31 @@
+function setup() {
+
+    let red = "#FF0000";
+    //need variables in the assignment
+    //or
+    let startX = 30;
+
+    createCanvas(800,600);
+    //noStroke(); 
+    //Can make the entire compostion have no strokes
+
+    background(207, 186, 27);
+    // If you put at the buttom of markup, it will over-ride all previous code
+
+    fill(22, 144, 219);
+    rect(70, 60, 300, 50,);
+    //creates a rectangle
+
+    stroke(96, 43, 255);
+    strokeWeight(7);
+    fill(214, 59, 24);
+    circle(100,100,30);
+    //creates a circle and changes the stroke weight as well as the color
+
+
+    strokeWeight(2);
+    line(10,10, 100,50);
+
+
+    
+}
