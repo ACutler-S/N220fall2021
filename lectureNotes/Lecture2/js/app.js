0cdb7132b8cd1a2570d0615fb94e61d457a30360
ACutler-S <ancutler@iu.edu>
2021-09-01 09:41:09 -0400

    InClassLectureNotesAdded

--- lectureNotes/Lecture2/js/app.js
+++ lectureNotes/Lecture2/js/app.js
@@ -1,3 +1,26 @@
+// ***** In Class lecture *****
+let sqColorR = 0; //use color picker stuff... not the color name.
+//one way we can use a variable
+let sqSize = 100;
+
 function setup(){
-    
+    createCanvas(600,400);
+    //rect(30,30,100,100);
 }
+
+function draw(){
+    fill(sqColorR,82,148);
+    rect(30, 30, sqSize, sqSize);
+    //sqSize = sqSize + 1;
+    //the above code makes the square grow
+    sqColorR = sqColorR + 1;  //Long hand
+    //sqColorR += 1 ; This is the short hand
+
+    if(sqColorR > 198){
+        sqColorR = 0;
+        
+    }
+
+}
+
+//********** video Lecture NOTES ***** */
