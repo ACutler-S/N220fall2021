407f7421ba7c04b8efcab884d15816697465bafe
ACutler-S <ancutler@iu.edu>
2021-09-08 14:05:53 -0400

    Newinlab

--- labs/InLabExercises/js/app.js
+++ labs/InLabExercises/js/app.js
@@ -19,6 +19,15 @@
     //}
 //}
 
-
+//--------------------------------------------------------------------------------------------------
 //Lab for week 2: Animation and Conditional Operations
 
+//Create an orange circle that mirrors the mouse and is in the opposite side of the canvas
+
+//Create a canvas 
+//store xcoord and ycoord as variables
+//After the stored coords, Create a circle that is mirrored to the cursor
+    //Could do this by flipping 
+
+    //circle( 400 - mouseX, 400 - mouseY, 25);
+    //THis is the math behind the problem
