4959f614dc89f8c3a4d3aefa34a9976774fe7f2c
ACutler-S <ancutler@iu.edu>
2021-09-09 11:25:16 -0400

    Commented all Assignments and all are working

--- labs/lab2/PuckSide/js/app.js
+++ labs/lab2/PuckSide/js/app.js
@@ -1,16 +1,31 @@
+//Andrew Cutler
+//N220 - 34971
+//September 8 ,2021
+//Assignment 2: Animation and Conditional Operators
+//Puck Side
+//----------------------------------------------------------------
+
+//Idea:
+//First create the circle and have it follow the mouse using mouseX and mouseY
+//Create the if else statements that will control the color change aspect
+
 function setup(){
     createCanvas(400, 300);   
 }
-
-
+//Creates the Canvas WOOOOAHHH breaking some ground here!
 
 function draw(){
     //background("grey");
-    circle(mouseX,mouseY, 30);3
+    circle(mouseX,mouseY, 30);
+//Creates the circle and makes it track the mouse
+
 
     if(mouseX >= 200){
-        fill("red");
+        fill(196, 35, 24);
     }else {
-        fill("blue");
-    }
+        fill(10, 181, 204);
+        }
+//Creates the if else statements that make the color change:
+//If the circle has an x coord greater or equal to 200, then fill it in red
+//Else, fill the circle in with blue
 }
--- labs/lab2/WorldWrap/js/app.js
+++ labs/lab2/WorldWrap/js/app.js
@@ -1,16 +1,32 @@
+//Andrew Cutler
+//N220 - 34971
+//September 8 ,2021
+//Assignment 2: Animation and Conditional Operators
+//World Wrap
+//----------------------------------------------------------------
+
+//Idea:
+//Create a circle that moves to the right at a speed of 5
+//When it gets to the edge of the canvas have it reset to the original point
+
 function setup(){
     createCanvas(800, 600);
 }
+//Creates the Canves, this one through me for a loop since it was 800 by 600 =)
+
 
 let xPos = 0;
+//Creates the variable that holds the initial speed of the ball
 
 function draw(){
     background("black");
     fill("blue");
     xPos += 5;
     circle(xPos,300,20);
+//Creates the circle and incriments the speed of the circle by 5
 
     if(xPos >= 800){
         xPos = 0;
     }
+//Creates the if statement that makes the ball reset after hitting the edge of the screen
 }
