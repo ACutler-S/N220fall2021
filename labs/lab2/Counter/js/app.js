18f44f3c531dbcb4d36d59cca63bf613de21dff8
ACutler-S <ancutler@iu.edu>
2021-09-09 11:16:36 -0400

    Comments to Counter

--- labs/lab2/Counter/js/app.js
+++ labs/lab2/Counter/js/app.js
@@ -1,9 +1,29 @@
+//Andrew Cutler
+//N220 - 34971
+//September 8 ,2021
+//Assignment 2: Animation and Conditional Operators
+//Counter
+//----------------------------------------------------------------
+
+//Idea:
+//Create a variable that holds the value 0
+//Incriment said variable by 1 for every frame
+//Output that variable to the console
+
+
 function setup(){
-    frameRate(60);
-    textSize(30);
     createCanvas(400, 400,);
 }
+//function setup is standard and nothing really interesting is here
+
+let frame = 0;
+//Create the variable that holds 0
+
 function draw(){
     background("green");
-    text(frameCount, width / 2, height / 2);
+//Green background so you have something to look at and are not staring at a white screen
+    frame++;
+//Incrimentation of variable
+    console.log(frame);
+//Output to the console
 }
