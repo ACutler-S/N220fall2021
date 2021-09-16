f6641ac76295ab7d3a2829dfdd7689e8b3e38502
ACutler-S <ancutler@iu.edu>
2021-09-16 12:43:34 -0400

    Completion of the assignment

--- labs/lab3/LoopComp/js/app.js
+++ labs/lab3/LoopComp/js/app.js
@@ -1,18 +1,41 @@
+//Andrew Cutler
+//N220 - 34971
+//September 16 ,2021
+//Assignment 3: Loops
+//Loop Comp
+//----------------------------------------------------------------
+
+//Idea: create a basic loop and use the output of that loop to manipulate perameters of different shapes
+
+
 function setup(){
     createCanvas(500,500);
-
+//Creation of the canvas
     for(let i = 0; i < 100; i++){
+//Creation of a basic loop
     circle(250 + i,250,10);
-    circle(250, 250 + i, 10);
-    circle(250-i,250,10);
-    circle(250, 250-i,10);
-    circle(250 % i, 250, 10);
-    circle(250, 250 % i, 10);
-    circle(250,250,10);
+//draws circle in the right direction
 
+    circle(250, 250 + i, 10);
+//draws circle in the down or positive y direction
+
+    circle(250-i,250,10);
+//draws circle in the left direction
+    circle(250, 250-i,10);
+//draws circle in the up or negative y direction
+    circle(250 % i, 250, 10);
+//draws circle in the far left side
+    circle(250, 250 % i, 10);
+//draws circle in the far upper side
+    circle(250,250,10);
+//draws circle in the base circel in the middle/ origin
     rect(10,10, 10 * i, 10);
+//upper bar on the top of the canvas
     rect(10, 10* i, 10, 10);
+//bar on the left side of the canvas
     rect(10* i,10*i,10*i,10*i);
+//increasing size of the square that goes across the middle
+
     }
 }
 
