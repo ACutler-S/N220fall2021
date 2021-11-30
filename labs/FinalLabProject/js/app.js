bc03b0778f0b168412f6368b4e5ab92d94898c0e
ACutler-S <ancutler@iu.edu>
2021-11-30 11:12:33 -0500

    planning for FinalProject

---
+++ labs/FinalLabProject/js/app.js
@@ -0,0 +1,6 @@
+/*
+Create the "grid";
+    make this out of grey boxes when clicked make the boxes into different colors
+Create events to make the two boxes change color
+Create the Div that stores who wins and who loses
+*/
